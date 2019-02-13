8edc06e8bfe2aa81ffe678c160ef1a06c22468ac
Ander D'nar <drdnar@gmail.com>
2019-02-13 15:48:18 -0500

    Add a simple program to test with

---
+++ src/testprgm/src/main.c
@@ -0,0 +1,21 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <tice.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <fileioc.h>
+#include "..\fontlibc\fontlibc.h"
+
+
+/* Main Function */
+void main(void) {
+    
+    
+    
+    /* Pause */
+    while (!os_GetCSC());
+}
